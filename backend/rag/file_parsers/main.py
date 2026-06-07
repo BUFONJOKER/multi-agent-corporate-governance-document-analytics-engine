d4005e7c4dba4e0dcdc3fb4ba79d10d69ecd9263
Abdul Rehman Javaid <abdulrehman9745@outlook.com>
2026-06-07 18:23:14 +0500

    fix: correct import path for pdf_parser and adjust function call

--- backend/rag/file_parsers/main.py
+++ backend/rag/file_parsers/main.py
@@ -1,4 +1,4 @@
-from backend.rag.file_parsers.pdf.main import pdf_parser
+from rag.file_parsers.pdf.main import pdf_parser
 from rag.file_parsers.csv.csv_parser import csv_parser
 
 
@@ -6,7 +6,7 @@ def parse_file(file_path: str, file_type: str):
     '''Parses a file based on its type.'''
 
     if file_type in 'pdf':
-        return pdf_parser(file_path, file_type)
+        return pdf_parser(file_path)
     elif file_type == "csv":
         return csv_parser(file_path)
     else:
